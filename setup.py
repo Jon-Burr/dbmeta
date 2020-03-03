d77c7386ba9fb48e7796ecd388d804d8f9ff2654
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-03-03 16:16:12 +0100

    Add funcsigs requirement for python version < 3.0

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ setup(
     url="https://github.com/Jon-Burr/dbobj.git",
     install_requires=[
         "future",
-        "jsonpatch"
+        "jsonpatch",
+        'funcsigs;python_version<"3.0"'
         ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
