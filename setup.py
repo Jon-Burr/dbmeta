3f2f1cad172f96cc1b634d8f1dbccdb13b0c91c9
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-03-04 15:44:47 +0100

    Fix dev status

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python",
